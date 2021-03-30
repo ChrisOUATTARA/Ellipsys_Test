0694b6f2e32d4d3303792533abb78f136bd1c2d8
chris <chrisesaieouattara@gmail.com>
2021-03-30 12:31:34 +0200

    small tab ok

--- src/main/java/org/example/MyApplication.java
+++ src/main/java/org/example/MyApplication.java
@@ -1,38 +1,27 @@
 package org.example;
 import java.sql.*;
+import java.util.ArrayList;
 
 public class MyApplication {
-	/*public static void connect() {
-		Connection conn = null;
-		try {
-			// db parameters
-			String url = "jdbc:sqlite:C:/Users/root/Downloads/ellipsys_test_db/ellipsys_test_db.db3";
-			// create a connection to the database
-			conn = DriverManager.getConnection(url);
 
-			System.out.println("Connection to SQLite has been established.");
-
-		} catch (SQLException e) {
-			System.out.println(e.getMessage());
-		} finally {
-			try {
-				if (conn != null) {
-					conn.close();
-				}
-			} catch (SQLException ex) {
-				System.out.println(ex.getMessage());
-			}
-		}
-	}*/
 	/**
 	 * @param args the command line arguments
 	 */
 	public static void main(String[] args) {
-		MyApplication app = new MyApplication();
 
-		//app.createNewTable("id");
-		//app.insert("id", "test");
-		app.selectAll();
+		String[] colNames = { "id", "trf", "tgtTb", "tgtLab", "srcTb", "srcLab"};
+		MyApplication app = new MyApplication();
+		for (String colName : colNames) {
+
+
+			app.createNewTable(colName);
+			ArrayList<String> colDatas = app.getElement(colName);
+			for (String colData : colDatas) {
+				app.insert(colName, colData);
+			}
+			app.selectAllOfTable(colName);
+		}
+
 	}
 	private Connection connect() {
 		// SQLite connection string
@@ -48,7 +37,7 @@ public class MyApplication {
 
 	public void selectAll(){
 		//String sql = "SELECT table_name FROM information_schema.tables WHERE table_type='BASE TABLE'";
-		String sql = "SELECT *  FROM oa_trf_src";
+		String sql = "SELECT tgtLab  FROM oa_trf_src group by tgtLab order by tgtLab asc";
 		//String sql = "SELECT *  FROM oa_trf_src_id_lkp";
 		try (Connection conn = this.connect();
 			 Statement stmt  = conn.createStatement();
@@ -56,19 +45,52 @@ public class MyApplication {
 
 			// loop through the result set
 			while (rs.next()) {
-				System.out.println(
-						//rs.getString("id")+"\t - " +rs.getString("champ"));
-				System.out.println(rs.getString("id") +  "\t - " +
+				System.out.println(rs.getString("tgtLab"));
+			/*	System.out.println(rs.getString("id") +  "\t - " +
 						rs.getString("trf") + "\t - " +
 						rs.getString("tgtTb")+ "\t - " +
 						rs.getString("tgtLab")+ "\t - " +
 						rs.getString("srcTb")+ "\t - " +
 						rs.getString("srcLab")+ "\t - " +
-						rs.getString("impact"));
+						rs.getString("impact"));*/
 			}
 		} catch (SQLException e) {
 			System.out.println(e.getMessage());
 		}
+	}
+
+	public void selectAllOfTable(String colName){
+		String sql = "SELECT *  FROM oa_trf_src_"+colName+"_lkp";
+		System.out.println("\n oa_trf_src_"+colName+"_lkp \n");
+		try (Connection conn = this.connect();
+			 Statement stmt  = conn.createStatement();
+			 ResultSet rs    = stmt.executeQuery(sql)){
+
+			// loop through the result set
+			while (rs.next()) {
+				System.out.println(rs.getString("id") +  "\t - " +rs.getString("champ"));
+			}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+
+	public ArrayList getElement( String colName){
+		//String sql = "SELECT table_name FROM information_schema.tables WHERE table_type='BASE TABLE'";
+		String sql = "SELECT "+colName+"  FROM oa_trf_src group by "+colName+" order by "+colName+" asc";
+		//String sql = "SELECT *  FROM oa_trf_src_id_lkp";
+		ArrayList<String> colData = new ArrayList<String>();
+		try (Connection conn = this.connect();
+			 Statement stmt  = conn.createStatement();
+			 ResultSet rs    = stmt.executeQuery(sql)){
+			while (rs.next()) {
+				colData.add(rs.getString(""+colName+""));
+			}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		return colData;
 	}
 
 	public static void createNewTable(String colName) {
